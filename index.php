2ce33240446e894ddfd3b941b391c0d7014e9175
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2021-08-28 02:12:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+include_once('telegrambot.php');
+include_once('simple_html_dom.php');
+$token = "1990222108:AAHwkxlU8PtdDruLVHgb2-Ka6qGD5eZ2xFQ";
+
+if (isset($data['callback_query'])) {
+	$callbdata = $data['callback_query'];
+	$chatid = $callbdata["from"]["id"];
+	if (count(explode('Durl:',$callbdata["data"]))==2){
+		$url = 'https://flibusta.is/'.explode('Durl:',$callbdata["data"])[1];
+		$ty = file_get_contents($url);
+		$titles = explode('</h1', explode('class="title">',$ty)[1])[0];
+		$download = explode('</div>',explode('скачать:', $ty)[1])[0];
+		$as = explode('<a', $download);
+		$keyboards = [];
+		foreach ($as as $a){
+			$aa = explode('</a>', $a)[0];
+			$title = explode('</a>', explode('>',$aa)[1])[0];
+			$href = explode('"',explode('href=', $aa)[1])[1];
+			$keyboards[count($keyboards)] = new_inline($title, 'callback_data', 'Downurl:'.$href.'|'.$titles.'.'.str_replace(')','',str_replace('(','',$title)));
+		}
+		sendMessage_inline($chatid, 'Скачать '.$titles.':', $token, $keyboards);
+	}
+	if (count(explode('Downurl:',$callbdata["data"]))==2){
+		$dwu = explode('Downurl:',$callbdata["data"])[1];
+		$urio = explode('|',$dwu);
+		$keyboards = [];
+		$keyboards[count($keyboards)] = new_inline('Скачать', 'url', ('https://request-kamakepar.herokuapp.com/?dw='.$urio[0].'&dwf='.$urio[1]));
+		sendMessage_inline($chatid, 'Нажмите кнопку скачать и не забудьте дать другое название книге и добавить расширение: ', $token, $keyboards);
+	}
+}
+
+if($msg == '/start'){
+	sendMessage($chatid, "Здравствуйте, вас приветствует бот бесплатных книг flibusta\nЧтобы найти книги, просто напишите название книги или автора",$token);
+}else{
+	if (count(explode('/downloadbookhref-', $msg)) == 2){
+		sendMessage($chatid, explode('/downloadbookhref-', $msg)[1], $token);
+	}else{
+		$f = file_get_contents('https://flibusta.is/booksearch?ask='.urlencode($msg));
+		//$html = str_get_html($f);
+		//$html = $html->find('#main')[0];
+		//$res = $html->find('ul li a');
+		$res = explode('Найденные', $f);
+		file_put_contents('me.hj', json_encode($res));
+		$start = 1;
+		$end = count($res);
+		while ($start < $end){
+			if (count(explode('книг',explode('</h3>',$res[$start])[0]))==2){
+				$keyboards = [];
+				$rst = explode('blockinner',$res[$start])[0];
+				$el = explode('<li>',$rst);
+				foreach ($el as $e){
+					$r = explode('</li>',$e)[0];
+					$href = explode('"',explode('href', $r)[1])[1];
+					$text = explode('</a>',explode('">', $r)[1])[0];
+					$text = str_replace('</b>','',str_replace('<b>', '', $text));
+					$keyboards[count($keyboards)] = new_inline($text, 'callback_data', 'Durl:'.$href);
+				} 
+				sendMessage_inline($chatid, 'Найденные '.explode('</h3>',$res[$start])[0], $token, $keyboards);
+			}
+			$start+=1;
+		}
+	}
+}
---
+++ php-parser.php
@@ -0,0 +1,66 @@
+<?php
+echo('<pre>');
+require('../../../configs/vt.php');
+require('../telegram_library.php');
+require('simple_html_dom.php');
+$url = 'http://kakoysegodnyaprazdnik.ru/';
+$domain = parse_url($url)['host'];
+$limit = 30;
+$count = 0;
+$links_1 = get_inner_links($url);
+$links_2 = [];
+foreach ($links_2 as $link) {
+	if ($count = $limit) {
+		break;
+	}
+	$count++;
+	$links_2[] = get_inner_links($link);
+}
+$links = array_merge($links_1, $links_2);
+
+function get_http_code($url) {
+	$headers = get_headers($url);
+	return (int) mb_substr($headers[0], 9, -3);
+}
+function get_inner_links($url) {
+	$domain = parse_url($url)['host'];
+	$html = new simple_html_dom();
+	$html->load_file($url);
+	$a = $html->find('a[href*='.$domain.']');
+	foreach ($a as $link) {
+		$html->load_file($link->href);
+		if (is_null($html->find('title', 0))) {
+			$title = NULL;
+		}
+		else {
+			$title = $html->find('title', 0);
+			$title = $title->plaintext;
+		}
+		if (is_null($html->find('meta[name="keywords"]', 0))) {
+			$keywords = NULL;
+		}
+		else {
+			$keywords = $html->find('meta[name="keywords"]', 0);
+			$keywords = $keywords->getAttribute('content');
+		}
+		$output[$link->href]= ['title'=>$title, 'keywords'=>$keywords];
+	}
+	// foreach ($links as $link) {
+	//     if (strpos($link, $domain) !== false) {
+	// 		$output[] = build_url($url);
+	//     }
+	// }
+	return $output;
+}
+function build_url($url) {
+	$components = parse_url($url);
+	if ($components == false) {
+		return false;
+	}
+	if (isset($components['query'])) {
+		return 'http://'.$components['host'].$components['path'].$components['query'];
+	}
+	else {
+		return 'http://'.$components['host'].$components['path'];
+	}
+}
